e1284ca86a6849cd9e1b422b134c49d238565742
kgaweda <kamil.gaweda@outlook.com>
2023-01-31 19:22:32 +0100

    add init commit

---
+++ src/main/java/com/example/javaexmaple/JavaExmapleApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaexmaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExmapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExmapleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaexmaple/JavaExmapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaexmaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExmapleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
